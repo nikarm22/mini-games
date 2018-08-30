75fa3bc476cbb0478e3827e0d8b132367a59167a
Armen Nikoyan <a.nikoyan@apollobytes.com>
2018-08-30 15:22:23 +0400

    Base stucture, webpack+typescript configuration

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Canvas Mini Games
 Small games implementation in JavaScript
+
+## Development
+Entry point of each game is `src/games/${game-name}/index.ts`
+To build a game run `npx webpack --env.game=${game-name}`
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = env => ({
+  entry: `./src/games/${env.game}/index.ts`,
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: `./${env.game}/bundle.js`,
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: 'src/shared/gamePage.html', to: `./${env.game}/index.html` },
+    ]),
+  ]
+});
